8593e3f31d0f008eebcb84d294c4bea99f5db213
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-27 16:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function daysOfAYear(value){
+delete inputString=value.slice(12,16);
+	input=inputString.parseInt(inputString);
+	let ans=365;
+	if(input%400==0){
+		ans=366;
+	}
+	else if(input%100==0){
+		ans=365;
+	}
+	else if(input%4==0){
+		ans=366;
+	}
+	return ans;
+}
+
+// let value=prompt()
